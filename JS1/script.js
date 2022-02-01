a9b34eb0ff669a84b7349cf81ab70e479c3f9b53
Muh.toyyib <akandeolalekantoheeb94@gmail.com>
2022-02-01 03:00:04 +0100

    Guess I'm at peace

---
+++ JS1/script.js
@@ -0,0 +1,7 @@
+alert("Hello! I have a message for you!");
+alert("This message will display is 3 seconds!");
+alert("1..");
+alert("2..");
+alert("3..");
+document.querySelector("h1").textContent = "Comrade! I know the world is fucked and everyone is struggling. I understand that you really trying your best but it seems everything is not working. Please don't stop, keep going, give thanks for life and be positive. surely, after hardship, there will surely be ease. I really love you Comrade💕💕";
+console.log('Are you satisfied coming here?');
